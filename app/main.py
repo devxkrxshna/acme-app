78ca386643f61a6464b9a776a279c90ad2dd36c4
Devi Krishna <devu@Devis-MacBook-Air.local>
2025-12-19 18:04:59 +0530

    basic FastAPI app with home page and Tailwind

---
+++ app/main.py
@@ -0,0 +1,16 @@
+# app/main.py
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(title="Acme Product Importer")
+
+# Mount static files and templates
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
